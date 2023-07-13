c1b973607b6e43fafcb09b6411e52cbf7080910b
Nicolas von Dolinger <109031315+nicolasvondolinger@users.noreply.github.com>
2023-07-13 11:10:41 -0300

    Add files via upload

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -2,44 +2,57 @@ package com.devsuperior.dslist.entities;
 
 import java.util.Objects;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_name")
+@Table(name = "tb_game")
 public class Game {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	 private Long id;
-	 private String title;
-	 
-	 @Column(name = "game_year")
-	 private Integer year;
-	 private String genre;
-	 private String platform;
-	 private String imgUrl;
-	 private String shortDescription;
-	 private String longDescription;
-	 
-	 public Game() {
-	 }
-	 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescription) {
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+	
+	public Game() {}
+
+	public Game(Long id, String title, Integer year, String genre, Double score, String platforms, String imgUrl,
+			String longDescription, String shortDescription) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
+		this.shortDescription = shortDescription;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
 	public Long getId() {
@@ -74,12 +87,12 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 	public String getImgUrl() {
@@ -90,12 +103,12 @@ public class Game {
 		this.imgUrl = imgUrl;
 	}
 
-	public String getShortDescription() {
-		return shortDescription;
+	public Double getScore() {
+		return score;
 	}
 
-	public void setShortDescription(String shortDescription) {
-		this.shortDescription = shortDescription;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getLongDescription() {
@@ -124,5 +137,5 @@ public class Game {
 	}
 	
 	
-	 
+	
 }
