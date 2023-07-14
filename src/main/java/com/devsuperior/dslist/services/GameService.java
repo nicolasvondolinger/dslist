468143c623ba55ce8fe2345a9385e7a0bec3b596
Nicolas von Dolinger <109031315+nicolasvondolinger@users.noreply.github.com>
2023-07-14 13:52:45 -0300

    Add files via upload

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -5,15 +5,24 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
 	
 	public List<GameMinDTO> findALL(){
 		List<Game> result = gameRepository.findAll();	
